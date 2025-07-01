dfd0181475802c7a80752af37a87df80d6751fa4
Shoccio <mynameismrj019@gmail.com>
2025-07-01 14:31:00 +0800

    added deleting of course and students in student courses

--- backend/functions/student_course_func.py
+++ backend/functions/student_course_func.py
@@ -15,6 +15,31 @@ def addEntry(student_id: str, program_id:str, db_connection: Session):
         db_connection.rollback()
         raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR, "Error occured adding corresponding courses")
     
+def editStudentCourses(new_id: str, old_id:str, db_connection: Session):
+    try:
+        db_connection.query(SC).filter_by(student_id = old_id).update({SC.student_id: new_id}, synchronize_session=False)
+    except SQLAlchemyError:
+        db_connection.rollback()
+        raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR, "Error occured editing student courses")
+    
+def deleteCourses(course_id: str, db_connection: Session):
+    try:
+        course = db_connection.query(SC).filter_by(course_id = course_id).first()
+
+        if not course:
+            raise HTTPException(status.HTTP_404_NOT_FOUND, "Course not found")
+        db_connection.delete(course)
+    except SQLAlchemyError:
+        raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR, "Error occured deleting course")
+    
+def deleteStudent(student_id: str, db_connection: Session):
+    try:
+        db_connection.query(SC).filter_by(student_id = student_id).delete(synchronize_session=False)
+    except:
+        db_connection.rollback()
+        raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR, "Error occured deleting student")
+
+    
 def getStudentCourses(student_id: str, db_connection: Session):
     try:
         courses = db_connection.query(SC).filter(SC.student_id == student_id).all()
