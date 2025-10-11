c69f6a626a34a32024ba82567d7f19c4ccfa5536
Shoccio <mynameismrj019@gmail.com>
2025-10-11 12:37:25 +0800

    Finished firestore functions for program collection

--- backend/functions/program_func.py
+++ backend/functions/program_func.py
@@ -1,7 +1,17 @@
 from models.programs_model import Program
 from sqlalchemy.orm import Session
 
+from db.firestore import fs
 
+#--------------------------Firestore Functions--------------------------
+def getProgramFirestore():
+    docs = fs.collection("programs").stream()
+
+    ids = [doc.to_dict()["id"] for doc in docs]
+
+    return ids
+
+#--------------------------MySQL Functions--------------------------
 def getProgram(db_connection: Session):
     ids = db_connection.query(Program).all()
     return ids
