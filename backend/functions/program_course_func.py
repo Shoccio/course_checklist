82b630751dee98c70b753b29a2560bdc996228cc
Shoccio <mynameismrj019@gmail.com>
2025-10-13 19:01:32 +0800

    final version before firestore migration

--- backend/functions/program_course_func.py
+++ backend/functions/program_course_func.py
@@ -3,6 +3,20 @@ from sqlalchemy.exc import SQLAlchemyError
 from fastapi import HTTPException, status
 from models.program_course_model import Program_Courses
 
+from db.firestore import fs
+
+#--------------------------Firestore Functions--------------------------
+def getCourseByProgramFirestore(program_id: str):
+    program_courses_collection = fs.collection("program_course")
+
+    courses = program_courses_collection.document(program_id).collection("courses").stream()
+
+    courses_dict = [course.to_dict() for course in courses]
+
+    return courses_dict
+
+#--------------------------MySQL Functions--------------------------
+
 def updateOrder(program_id: str, course_ids: list[str], db_connection: Session):
     try:
         for index, course_id in enumerate(course_ids):
